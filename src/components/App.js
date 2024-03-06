1835d72167466eadbadebbddf1c5f70415fef776
smhabibjr <smhabib.abir2030@gmail.com>
2024-03-07 00:33:33 +0100

    add home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
-
+import "../styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
 function App() {
   return (
-    <div className="App">
-      <div>Hello world!</div>
-    </div>
+    <Layout>
+      <Home />
+    </Layout>
   );
 }
 
