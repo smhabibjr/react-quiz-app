0d9979e0252602cc97a38e507c34ba78889e72f6
smhabibjr <smhabib.abir2030@gmail.com>
2024-03-07 21:30:36 +0100

    add result page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import "../styles/App.css";
 import Layout from "./Layout";
 /* import Login from "./pages/Login"
- */import Quiz from "./pages/Quiz";
+ import Quiz from "./pages/Quiz";
+ */
+import Result from "./pages/Result"
 function App() {
   return (
     <Layout>
-      <Quiz />
+      {/*       <Quiz />
+       */}
+       <Result />
     </Layout>
   );
 }
