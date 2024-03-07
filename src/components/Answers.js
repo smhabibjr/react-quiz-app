9cf2f07eb4d61ecb8ca3e185da640b0d577e0da2
smhabibjr <smhabib.abir2030@gmail.com>
2024-03-07 20:53:15 +0100

    add quiz page

---
+++ src/components/Answers.js
@@ -0,0 +1,9 @@
+import Checkbox from "../components/Checkbox"
+import classes from "../styles/Answers.module.css"
+export default function Answers() {
+    return (
+        <div className={classes.answers}>
+            <Checkbox className={classes.answer} text="Test answers" />
+        </div>
+    );
+  }
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import "../styles/App.css";
 import Layout from "./Layout";
-import Login from "./pages/Login"
+/* import Login from "./pages/Login"
+ */import Quiz from "./pages/Quiz";
 function App() {
   return (
     <Layout>
-      <Login />
+      <Quiz />
     </Layout>
   );
 }
