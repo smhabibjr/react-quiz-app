ad2ca681d1d45afa314d60362a65636d5c81f79a
smhabibjr <smhabib.abir2030@gmail.com>
2024-03-10 01:04:15 +0100

    add new one

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,23 +1,20 @@
+import { Link } from "react-router-dom";
+import useVideoList from "../hooks/useVideoList";
 import classes from "../styles/Videos.module.css";
 import Video from "./Video";
-import { Link } from "react-router-dom";
 export default function Videos() {
+  const { loading, error, videos } = useVideoList();
+  console.log(videos);
   return (
     <div className={classes.videos}>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-
-      <Link to="/quiz">
-        <Video />
-      </Link>
-
-      <Link to="/quiz">
-        <Video />
-      </Link>
+      {videos.map((video) => (
+        <Link to="/quiz" key={video.youtubeID}>
+          <Video key={video.youtubeID} title={video.title} id={video.youtubeID} noq={video.noq} />
+        </Link>
+      ))}
+      {!loading && videos.length === 0 && <div>No data found!</div>}
+      {error && <div>There was an error!</div>}
+      {loading && <div>Loading... </div>}
     </div>
   );
 }
---
+++ src/hooks/useVideoList.js
@@ -0,0 +1,44 @@
+import { get, getDatabase, orderByKey, query, ref } from "firebase/database";
+import { useEffect, useState } from "react";
+
+export default function useVideoList() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    async function fetchVideos() {
+      // db related works goes to be
+      const db = getDatabase();
+      const videosRef = ref(db, "videos");
+      const videoQuery = query(videosRef, orderByKey());
+
+      try {
+        setError(false);
+        setLoading(true);
+        // request db
+        const snapshot = await get(videoQuery);
+        setLoading(false);
+
+        if (snapshot.exists()) {
+            console.log(snapshot);
+          setVideos((prevvideos) => {
+            return [...prevvideos, Object.values(snapshot.val())];
+          });
+        } else {
+        }
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+      }
+    }
+
+    fetchVideos();
+  }, []);
+
+  return {
+    loading,
+    error,
+    videos
+  }
+}
