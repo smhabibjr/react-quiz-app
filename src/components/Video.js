cd4b921b4f4e5c693d4b6e29d178da11f8f8c506
smhabibjr <smhabib.abir2030@gmail.com>
2024-03-10 14:10:25 +0100

    videos page done

--- src/components/Video.js
+++ src/components/Video.js
@@ -9,7 +9,7 @@ export default function Video({ title, id, noq }) {
       <p>{title}</p>
       <div className={classes.qmeta}>
         <p>{noq} Questions</p>
-        <p>Total points :{noq * 5} </p>
+        <p>Total points : {noq * 5}</p>
       </div>
     </div>
   );
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -3,29 +3,41 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { Link } from "react-router-dom";
 import useVideoList from "../hooks/useVideoList";
 import Video from "./Video";
+import React from "react";
+
 export default function Videos() {
   const [page, setPage] = useState(1);
   const { loading, error, videos, hasMore } = useVideoList(page);
-
   console.log(videos);
   return (
     <div>
       {videos.length > 0 && (
         <InfiniteScroll
-          dataLength={videos.length}
-          hasMore={hasMore}
-          next={() => setPage(page + 8)}
-        >
-          {videos.map((video) => (
-            <Link to="/quiz" key={video.youtubeID}>
+        dataLength={videos.length}
+        hasMore={hasMore}
+        next={() => setPage(page + 8)}
+      >
+        {videos.map((video, index) => (
+          <React.Fragment key={`${video.youtubeID}-${index}`}>
+            {video.noq > 0 ? (
+              <Link to="/quiz">
+                <Video
+                  title={video.title}
+                  id={video.youtubeID}
+                  noq={video.noq}
+                />
+              </Link>
+            ) : (
               <Video title={video.title} id={video.youtubeID} noq={video.noq} />
-            </Link>
-          ))}
-        </InfiniteScroll>
+            )}
+          </React.Fragment>
+        ))}
+      </InfiniteScroll>
+      
       )}
       {!loading && videos.length === 0 && <div>No data found!</div>}
       {error && <div>There was an error!</div>}
-      {loading && <div>Loading... </div>}
+      {loading && <div>Loading...</div>}
     </div>
   );
 }
